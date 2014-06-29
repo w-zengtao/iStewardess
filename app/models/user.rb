9cd93812dd82c3cc6a631ad234ccb957f0f34c8d
Zeng WeiMao <so.zengtao@gmail.com>
2014-06-29 22:13:25 +0800

    nested

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   has_many  :user_resources
   
   mount_uploader :avatar,  MediaUploader
+  
+  accepts_nested_attributes_for :user_profile
+  accepts_nested_attributes_for :user_resources
 end
